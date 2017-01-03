196b2763f8da6163470e710f4b341f2714fa146d
Wisbell <westleyisbell@gmail.com>
2017-01-03 16:05:27 -0600

    Fixed products json and currently implementing adding fireworks to DOM

--- acme.js
+++ acme.js
@@ -33,7 +33,7 @@ categoriesPromise
         })
     .then(
         function(productsList){
-            products = productsList.products
+            products = productsList.products[0]
             console.log("products list promise resolved", products)
             return consoleLogJSON()
         })
@@ -73,3 +73,73 @@ function consoleLogJSON () {
     console.log(types)
     console.log(products)
 }
+
+
+function clearAddCategoryDiv () {
+    console.log("clearAddCategoryDiv function called")
+}
+
+function addFireworksCategoryToDOM () {
+    console.log("addFireworksCategoryToDOM function called")
+
+    clearAddCategoryDiv()
+
+    // Add Category Name
+    $(".addCategory").append(`<div class="row categoryName">
+                                <div class="col-md-12">${categories[0].name}</div>
+                              </div>`)
+
+    // Add Types/Products
+    for (var i = 0; i < types.length; i++) {
+
+        //var currentType = types[i].category
+
+        if (types[i].category != categories[0].id) {
+            break;
+        }
+
+        // Add current Type Name
+        $(".addCategory").append(`<div class="row typeName">
+                                    <div class="col-md-12">${types[i].name}</div>
+                                </div>`)
+
+        // Add Products of current type
+
+        for (var key in products) {
+            console.log("key", key)
+            console.log("object of current key", products[key])
+            console.log("current product type number", products[key].type)
+            console.log("current type id", types[i].id)
+
+            if (products[key].type != types[i].id) {
+                console.log("break")
+                break;
+            }
+
+            $(".addCategory").append(`<div class="row productName">
+                                        <div class="col-md-4">${key.name}</div>
+                                    </div>`)
+        }
+
+    }
+}
+
+function addExplosivesCategoryToDOM () {
+    console.log("addExplosivesCategoryToDOM function called")
+
+    clearAddCategoryDiv()
+}
+
+
+
+// Event Listeners
+
+$("#fireworksDropdown").click(addFireworksCategoryToDOM)
+
+$("#explosivesDropdown").click(addExplosivesCategoryToDOM)
+
+/*
+Once all data is loaded, you need to display the products in a Bootstrap grid.
+Each product must display the string name of its product type, and product category.
+Not the integer id value.
+*/
